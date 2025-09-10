7f6a1f1eac054966c136d9f4fcce44c8ed67fcc2
Rasika2511 <deshpanderasika7@gmail.com>
2025-09-10 13:24:02 +0000

    Wed, Sep 10, 2025, 3:24 PM +02:00

--- rps.py
+++ rps.py
@@ -1,50 +1,57 @@
 import random
 
-possible_options=['rock','paper','scissors']
-po=['r','p','s']
+possible_options = ["rock", "paper", "scissors"]
+po = ["r", "p", "s"]
+
 
 def user_input():
-    user_choice=input('Enter rock (or r), paper (or p) or scissors (or s): ').lower()
+    user_choice = input("Enter rock (or r), paper (or p) or scissors (or s): ").lower()
     if user_choice in possible_options or user_choice in po:
         return user_choice
     else:
-        print('Please enter one of the specified values')
+        print("Please enter one of the specified values")
         exit()
 
+
 def cpu_choice():
-    outcome=random.choice(possible_options)
-    print(f'Computer picked {outcome}')
+    outcome = random.choice(possible_options)
+    print(f"Computer picked {outcome}")
     return outcome
 
+
 def decide_winner(user: str, cpu: str):
-    winning_pairs={('rock','scissors'),('paper','rock'),('scissors','paper'),('r','scissors'),('p','rock'),('s','paper')}
-    draw_pairs={('r','rock'),('p','paper'),('s','scissors')}
-    return 'win' if (user,cpu) in winning_pairs else 'draw' if user==cpu or (user,cpu) in draw_pairs else 'lose'
+    winning_pairs = {
+        ("rock", "scissors"),
+        ("paper", "rock"),
+        ("scissors", "paper"),
+        ("r", "scissors"),
+        ("p", "rock"),
+        ("s", "paper"),
+    }
+    draw_pairs = {("r", "rock"), ("p", "paper"), ("s", "scissors")}
+    return (
+        "win"
+        if (user, cpu) in winning_pairs
+        else "draw"
+        if user == cpu or (user, cpu) in draw_pairs
+        else "lose"
+    )
 
-#user=user_input()
-#cpu=cpu_choice()
-#print(decide_winner(user,'paper'))
 
-results=[]
+results = []
 
-
-while len(results)<3:
-    user=user_input()
-    cpu=cpu_choice()
-    if decide_winner(user,cpu)=='draw':
+while len(results) < 3:
+    user = user_input()
+    cpu = cpu_choice()
+    if decide_winner(user, cpu) == "draw":
         pass
     else:
-        results.append(decide_winner(user,cpu))
+        results.append(decide_winner(user, cpu))
 
-print(results)
-if results.count('win')==2:
-    print('you win!')
+print(
+    f"In first game you {results[0]}. In second game you {results[1]}. In third game you {results[2]}."
+)
+if results.count("win") == 2:
+    print("Therefore, you win!")
 else:
-    print('you lose')
-
-
-
-
-
-
-
+    print("Therefore, you lose")
